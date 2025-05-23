2f984bc4e03fbf6df965a29d37a45e4005e02319
toannv0211 <nvtvlog234@gmail.com>
2025-05-23 11:35:34 +0700

    fix model

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import connectDB from "./config/db.js";
 import morgan from "morgan";
 import cors from "cors";
-import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
-dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-//log requests to the console
 app.use(morgan("dev"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
+
 connectDB()
     .then(() => {
         app.listen(PORT, () => {
