5b0e6ebe65a026fc4394b6f9a4e78482376e2cd8
toannv0211 <nvtvlog234@gmail.com>
2025-06-24 09:47:29 +0700

    Fix: Using AWS S3 for uploading file pdf and Add field CV Snapshot for preventing allow user update cv when applied

--- src/models/application.model.js
+++ src/models/application.model.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const ApplicationSchema = new mongoose.Schema({
     job: { type: mongoose.Schema.Types.ObjectId, ref: "Job" },
     cv: { type: mongoose.Schema.Types.ObjectId, ref: "CvProfile" },
+    cvSnapshot: { type: Object }, // <--- Thêm dòng này
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 
     applicationDate: { type: Date, default: Date.now },
